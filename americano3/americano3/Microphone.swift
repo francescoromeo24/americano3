4152651d00362e66ad6525364db7d01f7b2af968
francescoromeo24 <francesco24602@outlook.it>
2024-12-14 21:44:45 +0100

    text function, switch and import complete, checking for microphone

---
+++ americano3/americano3/Microphone.swift
@@ -0,0 +1,18 @@
+//
+//  Microphone.swift
+//  americano3
+//
+//  Created by Francesco Romeo on 14/12/24.
+//
+
+import SwiftUI
+
+struct Microphone: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Microphone()
+}
