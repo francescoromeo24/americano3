25759a74eeaa4efb341db798bb83b197ceab0f3f
francescoromeo24 <francesco24602@outlook.it>
2024-12-16 10:33:22 +0100

    creating speechrecognitionview, sill working on segmenting the content view before working on the single problems

---
+++ americano3/americano3/SpeechRecognitionView.swift
@@ -0,0 +1,18 @@
+//
+//  SpeechRecognitionView.swift
+//  americano3
+//
+//  Created by Francesco Romeo on 16/12/24.
+//
+
+import SwiftUI
+
+struct SpeechRecognitionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeechRecognitionView()
+}
