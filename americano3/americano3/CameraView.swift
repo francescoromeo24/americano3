9f9dc847e58ba93b94f69fdf0b146c1139c8a540
francescoromeo24 <francesco24602@outlook.it>
2024-12-31 13:21:48 +0100

    integration favourites view and camera

---
+++ americano3/americano3/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  americano3
+//
+//  Created by Francesco Romeo on 31/12/24.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
---
+++ americano3/americano3/models/VisionProcessor.swift
@@ -0,0 +1,8 @@
+//
+//  VisionProcessor.swift
+//  americano3
+//
+//  Created by Francesco Romeo on 31/12/24.
+//
+
+import Foundation
