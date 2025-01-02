9ac7dcce8dd34a8b7f58b198498fcd44cd8fae41
francescoromeo24 <francesco24602@outlook.it>
2025-01-03 00:00:46 +0100

    flashcard click

---
+++ americano3/americano3/FlashcardDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FlashcardDetailView.swift
+//  americano3
+//
+//  Created by Francesco Romeo on 02/01/25.
+//
+
+import SwiftUI
+
+struct FlashcardDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlashcardDetailView()
+}
